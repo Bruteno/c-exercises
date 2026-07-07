167164c517fd0c40c31b7da0b357605638773d6f
Bruteno <ivars.levans@gmail.com>
2026-07-07 17:34:12 +0300

    1.11->1.12

---
+++ 1.0/1.12.c
@@ -0,0 +1,27 @@
+/*
+  Exercise 1-12. Write a program that prints its input one word per line.
+*/
+
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+    int c, state;
+    
+    state = OUT;
+    
+    while ((c = getchar()) != EOF) {
+	if (c != ' ' && c != '\n' && c != '\t') {
+	    state = IN;
+	    putchar(c);
+	}
+	else if (state == IN) {
+	    state = OUT;
+	    putchar('\n');
+	}
+    }
+
+    return 0;
+}
